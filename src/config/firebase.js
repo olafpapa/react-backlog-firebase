bd02d60ca83b8b865954fea09ac60b3bbfa4683b
Shinichiro Iwatsuru <shinichiro.iwatsuru@inte.co.jp>
2019-06-25 19:25:41 +0900

    firebase初期化。

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "***REMOVED***",
+  authDomain: "react-todo-firebase-8556e.firebaseapp.com",
+  databaseURL: "https://react-todo-firebase-8556e.firebaseio.com",
+  projectId: "react-todo-firebase-8556e",
+  storageBucket: "",
+  messagingSenderId: "331889090881",
+  appId: "1:331889090881:web:2463244552b32756"
+};
+
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase;
