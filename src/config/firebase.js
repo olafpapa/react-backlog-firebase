cf10756cf371ae26312086e78a1e14b830ff28ed
olafpapa <olafpapa@iwatsurishinichirounoMacBook-Pro.local>
2019-06-26 00:16:16 +0900

    setup Firebase.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 export const firebaseConfig = {
-  apiKey: "***REMOVED***",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: "react-todo-firebase-8556e.firebaseapp.com",
   databaseURL: "https://react-todo-firebase-8556e.firebaseio.com",
   projectId: "react-todo-firebase-8556e",
   storageBucket: "",
-  messagingSenderId: "331889090881",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: "1:331889090881:web:2463244552b32756"
 };
 
