3607792d0b08c4fe8d9a8ad01804a8b8949da0ba
Shinichiro Iwatsuru <shinichiro.iwatsuru@inte.co.jp>
2019-07-10 16:25:26 +0900

    addDecoratorでstyleにpaddingを追加する。

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,6 +4,7 @@ import ProjectSummary from '../components/projects/ProjectSummary'
 
 
 storiesOf('projects', module)
+  .addDecorator(story => <div style={{ padding: '1rem' }}>{story()}</div>)
   .add('ProjectSummary', () => (
     <ProjectSummary project={ {
       title: 'タイトル',
