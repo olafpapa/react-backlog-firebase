a63803cdf16a204e045b6b2ff52bb931fdf6e23d
olafpapa <olaf0daisuki>
2019-07-20 18:07:09 +0900

    ProjectSummaryのテスト。

---
+++ src/__tests__/components/projects/ProjectSummary.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+import ProjectSummary from '../../../components/projects/ProjectSummary'
+
+describe('<ProjectSummary />', () => {
+
+  const project = {
+    id: 1, title: 'タイトル', authorFirstName: '太郎', authorLastName: '山田'
+  }
+
+  it('表示確認', () => {
+    // テスト用のdata-test属性
+    const sel = id => `[data-test="${ id }"]`
+
+    const wrapper = shallow(<ProjectSummary project={ project }/>)
+
+    expect(wrapper.find(sel('title')).length).toBe(1);
+    expect(wrapper.find(sel('title')).text()).toEqual('タイトル');
+
+    expect(wrapper.find(sel('name')).length).toBe(1);
+    expect(wrapper.find(sel('name')).text()).toEqual('Posted by 太郎 山田');
+
+  })
+
+});
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import moment from 'moment'
 
 export const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">{ project.title }</span>
-        <p>Posted by { project.authorFirstName } { project.authorLastName }</p>
+        <span data-test="title" className="card-title">{ project.title }</span>
+        <p data-test="name">Posted by { project.authorFirstName } { project.authorLastName }</p>
         <p className="grey-text">{ project.createdAt ? moment(project.createdAt.toDate()).calendar() : null }</p>
       </div>
     </div>
